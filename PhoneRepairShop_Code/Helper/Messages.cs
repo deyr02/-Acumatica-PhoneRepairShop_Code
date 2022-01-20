03f6eec41f28ddd59cf97d42d5cd0b6bcaf2be2e
DEY RAJIB <rade002@ess.ais.ac.nz>
2022-01-21 09:22:21 +1300

    New form RS202000 added

---
+++ PhoneRepairShop_Code/Helper/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneRepairShop.Helper
+{
+    public static class Messages
+    {
+        //complexity of repair
+        public const string High = "High";
+        public const string Medium = "Medium";
+        public const string Low = "Low";
+
+    }
+}
