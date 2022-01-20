ea1c23f3cf5f148fbbd7a29764809a71e9fc75ac
DEY RAJIB <rade002@ess.ais.ac.nz>
2022-01-20 14:44:55 +1300

    Generic event handler

--- PhoneRepairShop_Code/RSSVRepairServiceMaint.cs
+++ PhoneRepairShop_Code/RSSVRepairServiceMaint.cs
@@ -6,10 +6,10 @@ namespace PhoneRepairShop
   public class RSSVRepairServiceMaint : PXGraph<RSSVRepairServiceMaint>
   {
 
-    protected void RSSVRepairService_WalkInService_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+    protected void _(Events.FieldUpdated<RSSVRepairService, RSSVRepairService.walkInService> e)
     {
       
-      var row = (RSSVRepairService)e.Row;
+      var row = e.Row;
       if (row.WalkInService == true) 
       { 
         row.PreliminaryCheck = false; 
@@ -21,9 +21,22 @@ namespace PhoneRepairShop
       
     }
 
-    
+        protected void _(Events.FieldUpdated<RSSVRepairService, RSSVRepairService.preliminaryCheck> e) 
+        {
+            var row = e.Row; 
+            if (row.PreliminaryCheck == true) 
+            { 
+                row.WalkInService = false; 
+            } 
+            else 
+            { 
+                row.WalkInService = true; 
+            } 
+        }
 
-    public PXSave<RSSVRepairService> Save;
+
+
+        public PXSave<RSSVRepairService> Save;
     public PXCancel<RSSVRepairService> Cancel;
     public SelectFrom<RSSVRepairService>.View RepairService;
 
