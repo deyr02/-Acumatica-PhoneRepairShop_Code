0d2e708c1c8235fc8bababdb3fb2f47726163720
DEY RAJIB <rade002@ess.ais.ac.nz>
2022-01-25 10:31:04 +1300

    updateing fields on update of another field

--- PhoneRepairShop_Code/RSSVRepairPriceMaint.cs
+++ PhoneRepairShop_Code/RSSVRepairPriceMaint.cs
@@ -45,31 +45,55 @@ namespace PhoneRepairShop
             } 
         }
 
+
+
         //Update the IsDefault field of other records with the same repair item type 
         //when the IsDefault field is updated.
-        protected void _(Events.RowUpdated<RSSVRepairItem> e) 
+        protected void _(Events.RowUpdated<RSSVRepairItem> e)
+        {
+            if (e.Cache.ObjectsEqual<RSSVRepairItem.isDefault,
+                                     RSSVRepairItem.required>(e.Row, e.OldRow)) return;
+
+            RSSVRepairItem row = e.Row;
+            //Use LINQ to select the repair items 
+            // with the same repair item type as in the updated row.
+            var repairItems = RepairItems.Select()
+                .Where(item => item.GetItem<RSSVRepairItem>().RepairItemType == row.RepairItemType);
+            foreach (RSSVRepairItem repairItem in repairItems)
+            {
+                if (repairItem.LineNbr == row.LineNbr) continue;
+
+                //Set IsDefault to false for all other items.
+                if (row.IsDefault == true && repairItem.IsDefault == true)
+                {
+                    repairItem.IsDefault = false;
+                    RepairItems.Update(repairItem);
+                }
+                //Make the Required field identical for all items.
+                if (row.Required != e.OldRow.Required && repairItem.Required != row.Required)
+                {
+                    repairItem.Required = row.Required;
+                    RepairItems.Update(repairItem);
+                }
+            }
+            //Refresh the UI.
+            RepairItems.View.RequestRefresh();
+        }
+
+
+
+        //Update the Required check box when a repair item type is selected.
+        protected void _(Events.FieldUpdated<RSSVRepairItem, RSSVRepairItem.repairItemType> e) 
         { 
             RSSVRepairItem row = e.Row; 
-            //Use LINQ to select the repair items with the same repair item type 
-            //as in the updated row.
-            var repairItems = RepairItems.Select()
-                .Where(item => item.GetItem<RSSVRepairItem>()
-                .RepairItemType == row.RepairItemType); 
-            
-            foreach (RSSVRepairItem repairItem in repairItems) 
-            { 
-                if (repairItem.LineNbr != row.LineNbr) 
-                { 
-                    //Set IsDefault to false for all other items.
-                    if (row.IsDefault == true) 
-                    { 
-                        repairItem.IsDefault = false; 
-                        RepairItems.Update(repairItem); 
-                    } 
-                } 
-            } 
-            //Refresh the UI.
-            RepairItems.View.RequestRefresh(); 
+            //Use LINQ to check whether there are any repair items with the same 
+            //repair item type.
+            var repairItem = (RSSVRepairItem)RepairItems.Select()
+                .Where(item => item.GetItem<RSSVRepairItem>().RepairItemType == row.RepairItemType)
+                . FirstOrDefault(); 
+            //Copy the Required value from the previous records.
+            if (repairItem != null) 
+                row.Required = repairItem.Required; 
         }
 
         #endregion
